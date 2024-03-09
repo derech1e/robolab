8d37c2609c103d5dc44c7a96f6148866367d9a69
derech1e <nurkthomas@gmail.com>
2024-03-09 17:55:40 +0100

    tuned pid follow algorithm

--- src/follow.py
+++ src/follow.py
@@ -21,14 +21,14 @@ class Follow:
         self.last_error = 0
 
         # TODO: tune these values
-        self.normalSpeed = 200
-        self.KA = 1 # to skale all
-        self.KP = 0.75
-        self.KI = 0
-        self.KD = 0
-        self.KEEP_INTEGRAL = 1
+        self.normalSpeed = 150
+        self.KA = 1  # to skale all
+        self.KP = 0.51
+        self.KI = 0.074
+        self.KD = 0.45
+        self.INTEGRAL_FACTOR = 0.587
 
-        #test
+        # test
         self.state = False
         self.sensorT = TouchSensor()
         self.dataPlot = []
@@ -36,23 +36,19 @@ class Follow:
     # 350 = WHITE
     # 50 = BLACK
 
-    def calc_error(self) -> tuple[float,float]:
+    def calc_error(self) -> tuple[float, float]:
         color = self.color_sensor.get_color_hls()
         error = self.avrLightness - color[1]
 
-        #update integral (erhoet sich um 70)
-        self.integral_value = self.KEEP_INTEGRAL * self.integral_value + error
+        # update integral (erhoet sich um 70)
+        self.integral_value = self.INTEGRAL_FACTOR * self.integral_value + error
         delta_error = error - self.last_error
         self.last_error = error
         return error, delta_error
 
-
     def calc_turn(self) -> float:
         error, delta_error = self.calc_error()
-        # print(f"error: {error}, p-part: {error*self.KP}, i-part: {self.KI * self.integral_value}, d-part: {self.KD * delta_error}")
-        # self.dataPlot.append(f"{time.time()},{error*self.KP},{self.KI * self.integral_value},{delta_error}")
-        self.dataPlot.append((time.time(), error*self.KP, self.KI * self.integral_value, self.KD * delta_error, self.KP * error + self.KI * self.integral_value + self.KP * delta_error))
-        return self.KA * (self.KP * error + self.KI * self.integral_value + self.KP * delta_error)
+        return self.KA * (self.KP * error + self.KI * self.integral_value + self.KD * delta_error)
 
     def calc_speed_left(self) -> int:
         # print(f"Calc turn: {self.calc_turn()}")
@@ -62,41 +58,8 @@ class Follow:
         return self.normalSpeed - int(self.calc_turn())
 
     def follow(self):
-        # print("Init driving forward")
-        # self.motor_sensor.drive(self.normalSpeed, self.normalSpeed)
-        # print("Driving forward")
-
-        while True:
-            if self.color_sensor.get_hls_color_name() == "red" or self.color_sensor.get_hls_color_name() == "blue":
-                self.motor_sensor.stop()
-                self.speaker_sensor.play_beep()
-                print(self.color_sensor.get_hls_color_name())
-                # time.sleep(2)
-                # self.motor_sensor.forward(self.calc_speed_left(), self.calc_speed_right())
-
+        if self.color_sensor.get_hls_color_name() == "red" or self.color_sensor.get_hls_color_name() == "blue":
+            self.motor_sensor.stop()
+            print("Motor stopped")
+        else:
             self.motor_sensor.forward(self.calc_speed_left(), self.calc_speed_right())
-
-            # while self.sensorT.is_pressed():
-            #     self.state = True
-            #     pass
-            # if self.state:
-            #     self.KP += 0.05
-            #     self.dataPlot = []
-            #     print(f"KI: {self.KI}")
-            #     self.state = False
-            #
-        #writing data to csv
-            # if self.sensorT.is_pressed():
-            #     break
-
-        # print("writing")
-        # with open('sensors/data.csv', 'w', newline='') as csvfile:
-        #     writer = csv.writer(csvfile)
-        #     writer.writerow(["t", f"KP={self.KP}", f"KI={self.KI}", f"KD={self.KD}","turn"])
-        #     writer.writerows(self.dataPlot)
-        # print("finished writing")
-
-
-            
-
-            
