a25051a013787b25cb1b13c1f05b40d6877ea721
derech1e <nurkthomas@gmail.com>
2024-03-10 16:33:37 +0100

    add collision detection

--- src/follow.py
+++ src/follow.py
@@ -5,13 +5,14 @@ from sensors.color_sensor import *
 from sensors.motor_sensor import *
 from sensors.speaker_sensor import SpeakerSensor
 from sensors.touch_sensor import *
+from sensors.sonar_sensor import SonarSensor
 
 
 class Follow:
     def __init__(self) -> None:
         self.motor_sensor = MotorSensor()
+        self.sonar_sensor = SonarSensor()
         self.color_sensor = ColorSensor()
-        self.speaker_sensor = SpeakerSensor()
         self.colorData = self.color_sensor.return_color()
         # self.avrColor = (self.colorData["white"][0] + self.colorData["white"][1] + self.colorData["white"][2] \
         #                 + self.colorData["black"][0] + self.colorData["black"][1] + self.colorData["black"][2]) // 2
@@ -23,15 +24,18 @@ class Follow:
         # TODO: tune these values
         self.normalSpeed = 150
         self.KA = 1  # to skale all
-        self.KP = 0.51
-        self.KI = 0.074
-        self.KD = 0.45
-        self.INTEGRAL_FACTOR = 0.587
+        self.KP = 0.51  # Aktueller Fehler ??
+        self.KI = 0.074  # Bisherige Fehler ??
+        self.KD = 0.45  # Zukünftige Fehler ??
+        self.INTEGRAL_FACTOR = 0.587  # In kurven??
 
         # test
         self.state = False
         self.sensorT = TouchSensor()
         self.dataPlot = []
+
+        # Collision detection
+        self.collision_turn_in_progress = False
 
     # 350 = WHITE
     # 50 = BLACK
@@ -48,7 +52,7 @@ class Follow:
 
     def calc_turn(self) -> float:
         error, delta_error = self.calc_error()
-        return self.KA * (self.KP * error + self.KI * self.integral_value + self.KD * delta_error)
+        return self.KP * error + self.KI * self.integral_value + self.KD * delta_error
 
     def calc_speed_left(self) -> int:
         # print(f"Calc turn: {self.calc_turn()}")
@@ -57,9 +61,28 @@ class Follow:
     def calc_speed_right(self) -> int:
         return self.normalSpeed - int(self.calc_turn())
 
+    def turn_until_line_detected(self):
+        self.collision_turn_in_progress = True
+        self.motor_sensor.turn_relative_angle(150, 200)
+        self.motor_sensor.beyblade(-100)
+        while True:
+            color = self.color_sensor.get_hls_color_name()
+            time.sleep(0.1)
+            if color == "black":
+                self.motor_sensor.stop()
+                self.collision_turn_in_progress = False
+                print("Black line detected")
+                break
+
+    def stop(self):
+        self.motor_sensor.stop()
+
     def follow(self):
+        # print(self.color_sensor.get_color_hls())
+        # time.sleep(5)
         if self.color_sensor.get_hls_color_name() == "red" or self.color_sensor.get_hls_color_name() == "blue":
             self.motor_sensor.stop()
             print("Motor stopped")
+            time.sleep(3)
         else:
             self.motor_sensor.forward(self.calc_speed_left(), self.calc_speed_right())
--- src/robot.py
+++ src/robot.py
@@ -1,7 +1,11 @@
+import time
+
 from odometry import Odometry
 from planet import Planet
 from follow import Follow
 from sensors.touch_sensor import TouchSensor
+from sensors.sonar_sensor import SonarSensor
+from sensors.speaker_sensor import SpeakerSensor
 
 
 class Robot:
@@ -13,9 +17,12 @@ class Robot:
         self.follow = Follow()
 
         self.button = TouchSensor()
+        self.sonar_sensor = SonarSensor()
+        self.speaker_sensor = SpeakerSensor()
 
         self.active = True
         self.button_pressed = False
+        self.collision_detected = False
 
     def robot(self):
 
@@ -27,4 +34,16 @@ class Robot:
         # TODO: Impl color calibration before running
 
         while self.active:
-            self.follow.follow()
+
+            if self.sonar_sensor.is_colliding() and not self.collision_detected:
+                self.speaker_sensor.play_tone()
+                self.follow.stop()
+                self.collision_detected = True
+
+            if not self.collision_detected and not self.follow.collision_turn_in_progress:
+                self.follow.follow()
+            else:
+                if not self.follow.collision_turn_in_progress:
+                    self.follow.turn_until_line_detected()
+                    self.collision_detected = False
+
