7fea6aa8061b1061d5fdf1d162421e851fc9dc26
derech1e <nurkthomas@gmail.com>
2024-03-14 09:24:38 +0100

    add time delay to sonar_sensor

--- src/sensors/sonar_sensor.py
+++ src/sensors/sonar_sensor.py
@@ -1,3 +1,6 @@
+import math
+import time
+
 import ev3dev.ev3 as ev3
 
 
@@ -6,10 +9,15 @@ class SonarSensor:
     def __init__(self):
         self.ultraSonic = ev3.UltrasonicSensor()
         self.ultraSonic.mode = 'US-DIST-CM'  # Continuous measurement in centimeters (for inch use US-DIST-IN)
+        self.last_check = -1
+        self.prev_value = math.inf
         # ultraSonic.mode = 'US-SI-CM'  # Single measurement in centimeters (for inch use US-SI-IN)
 
     def distance(self):
-        return self.ultraSonic.value()
+        if self.last_check > time.time_ns() + 150:  # Add execution delay
+            self.prev_value = self.ultraSonic.value()
+            self.last_check = time.time_ns()
+        return self.prev_value
 
     def is_colliding(self):
         return self.distance() < 210  # Colliding range
