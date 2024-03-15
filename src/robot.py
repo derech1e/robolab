9a6287998c2c5769339ac8036e4677c7dc28d341
femu072f <felix.mueller3@mailbox.tu-dresden.de>
2024-03-15 16:15:51 +0100

    changed turn angle to align from __start_node

--- src/robot.py
+++ src/robot.py
@@ -175,9 +175,9 @@ class Robot:
             if not stop_reason == StopReason.COLLISION:  # TODO: Improve this remove
                 # self.motor_sensor.turn_angle_blocking(abs(self.__current_node[1].value - self.__next_node[1].value)) # Subtract angle to get relative rotation to current position
                 # self.motor_sensor.turn_angle(20)
-                turn_angle = ((self.__current_node[1].value + 180) % 360) - self.__next_node[1].value
+                turn_angle = ((self.__current_node[1].value + 180) % 360) - self.__start_node[1].value
                 if stop_reason == StopReason.FIRST_NODE:
-                    turn_angle = ((self.__current_node[1].value + 180) % 360) - self.__next_node[1].value
+                    turn_angle = ((self.__current_node[1].value + 180) % 360) - self.__start_node[1].value
                 print(turn_angle)
                 self.motor_sensor.turn_angle(turn_angle)
                 # self.motor_sensor.drive_cm(5, 5, 100)
