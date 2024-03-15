c655a6a6f2c082f8a037bb5421d539e7e31c7094
fipe472e <finn.pecher@mailbox.tu-dresden.de>
2024-03-15 12:59:41 +0100

    tryed to fix bug where the rover is not turning enought

--- src/driver.py
+++ src/driver.py
@@ -70,7 +70,7 @@ class Driver:
         self.motor_sensor.turn_angle(-30)
         time.sleep(0.3)
 
-        alpha = -0.5
+        alpha = -0.5 #about 30deg
         angle = 0
         directions: [Direction] = []
         old_pos = (self.motor_sensor.beyblade(0))
@@ -78,21 +78,22 @@ class Driver:
         #dont scan the path behind you
         for i in [0, 1, 3, 4]:
             angle = math.pi * i / 2
-            print(f"current angle: {angle}, real angle: {alpha}")
-            while alpha < angle + (0 if i == 4 else 0.3):
+            # print(f"current angle: {angle}, real angle: {alpha}")
+            while alpha < angle + (0 if i == 4 else 0.4):
                 self.motor_sensor.beyblade(150 * (angle-alpha if i ==4 else 1 ))
                 new_pos = self.motor_sensor.beyblade(150)
                 delta_pos = (new_pos[0] - old_pos[0], new_pos[1] - old_pos[1])
                 old_pos = new_pos
                 alpha = alpha + (delta_pos[1] - delta_pos[0]) / constants.AXLE_LENGTH * 0.05
                 #print(self.color_sensor.get_color_name())
-                print(self.color_sensor.get_luminance())
-                if (self.color_sensor.get_luminance() < 85 and alpha > angle - (0 if i == 4 else 0.3)):
-                        # and not self.color_sensor.get_color_name()):
-                    dir = ((incoming_direction.value + 360 - 90 * i) % 360)
-                    directions.append(Direction(dir))
-                    print(f"path detected direction: {angle}")
-                    break
+                # print(self.color_sensor.get_luminance())
+                if not i == 4:
+                    if (self.color_sensor.get_luminance() < 85 and alpha > angle - 0.3):
+                            # and not self.color_sensor.get_color_name()):
+                        dir = ((incoming_direction.value + 360 - 90 * i) % 360)
+                        directions.append(Direction(dir))
+                        print(f"path detected direction: {angle}")
+                        break
 
         self.motor_sensor.stop()
         directions = set(directions)
