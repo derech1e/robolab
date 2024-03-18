8516f80cb9c52f35444bb34b5be779634a84a5eb
femu072f <felix.mueller3@mailbox.tu-dresden.de>
2024-03-18 21:09:36 +0100

    added comments for control_button and pid

--- src/pid.py
+++ src/pid.py
@@ -15,12 +15,18 @@ class PID:
     def __get_brightness_error(self) -> float:
         """
         Returns the brightness error of the current following path
+
         :return: float
         """
         average_lightness = (self.color_sensor.color_data["white"][1] + self.color_sensor.color_data["black"][1]) / 2  # calc the average lightness
         return average_lightness - self.color_sensor.get_luminance()
 
     def calc_error(self) -> tuple[float, float]:
+        """
+        Calculates integral value and delta error from brightness error
+
+        :return: tuple[float, float]
+        """
         error = self.__get_brightness_error()
         self.integral_value = constants.INTEGRAL_FACTOR * self.integral_value + error
         delta_error = error - self.last_error
@@ -28,11 +34,20 @@ class PID:
         return error, delta_error
 
     def calc_turn(self) -> float:
+        """
+        Calculates turn value from current error values
+        """
         error, delta_error = self.calc_error()
         return round(constants.KP * error + constants.KI * self.integral_value + constants.KD * delta_error)
 
     def calc_speed(self) -> Tuple[int, int]:
+        """
+        Calculates speed for left and right motors based on calculated turn value
+        """
         return constants.SPEED + self.calc_turn(), constants.SPEED - self.calc_turn()
 
     def stop(self) -> None:
+        """
+        Stops the motor
+        """
         self.motor_sensor.stop()
